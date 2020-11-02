6bfbd177b8e2525266177fc2b5dca9eb76c469c1
iostream17 <17309940+iostream17@users.noreply.github.com>
2020-11-02 18:55:39 +0200

    next

---
+++ update_table/step_12.sql
@@ -0,0 +1,4 @@
+UPDATE  reg_11_02_20_v2
+
+SET phoneinternal =  replace(replace(replace(replace(replace(phoneinternal,'Ţ','T')
+,'ţ','t'),'Ş','S'),'ş','s'),'ă','a');
---
+++ update_table/step_14.sql
@@ -0,0 +1,4 @@
+UPDATE  reg_11_02_20_v2 
+SET phoneinternal = 'activ'
+WHERE 
+phoneinternal <>  'lichidata';
