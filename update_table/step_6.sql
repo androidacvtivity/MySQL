1a8102ed8328acbb3886761cb32afbb833872c56
iostream17 <17309940+iostream17@users.noreply.github.com>
2020-11-02 18:30:37 +0200

    Summary

---
+++ update_table/step_6.sql
@@ -0,0 +1,42 @@
+SET @row_number = 0;
+	 
+	INSERT INTO id_registru_md_09_01_2020 (
+id,
+IDNO,
+DATA_REG,
+DEN_COM,
+FORMA_ORG,
+ADRESA,
+LIST_COND,
+LISTA_FOND,
+GEN_ACT_NE_LIC,
+GEN_ACT_LIC,
+STATUTUL
+
+ ) 
+	  SELECT 
+	 @row_number:=@row_number+1  AS id, 
+	 IDNO,
+	 DATA_REG,
+	 DEN_COM,
+	 FORMA_ORG,
+	 ADRESA,
+	 LIST_COND,
+	 LISTA_FOND,
+	 GEN_ACT_NE_LIC,
+	 GEN_ACT_LIC,
+	 STATUTUL 
+	
+		 
+		 
+		 
+		 FROM  vw_1
+		 
+		 
+		 
+		 WHERE 
+		 1=1
+
+		 
+		 ORDER BY 
+		 DATA_REG DESC
