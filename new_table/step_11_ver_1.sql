facb805d3550a68cb10b1f0708e11b8d29d5ea52
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-05-26 09:51:29 +0300

    registru_md_05_23_2022 #66
    registru_md_05_23_2022 #66

--- new_table/step_11_ver_1.sql
+++ new_table/step_11_ver_1.sql
@@ -1,6 +1,6 @@
 SET @row_number = 0;
 	 
- 	INSERT INTO id_registru_mprod_05_16_22 (
+ 	INSERT INTO id_registru_mprod_05_23_22 (
  	id,
   IDNO,
  	 DATA_REG,
--- new_table/step_7_ver_1.sql
+++ new_table/step_7_ver_1.sql
@@ -12,7 +12,7 @@ DATE_OF_LIQUID,
 STATUTUL_DATE,
 STATUTUL
 
-FROM registru_md_05_16_2022
+FROM registru_md_05_23_2022
 
 
 
