c79cd6c666e59d500d1cb5fd26a41bf4b359e0b6
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2023-03-25 10:23:31 +0200

    2023.03.20 Company #161

--- new_table/step_15_ver_1.sql
+++ new_table/step_15_ver_1.sql
@@ -1,4 +1,4 @@
-INSERT INTO reg_03_14_23_t1 (
+INSERT INTO reg_03_20_23_t1 (
 id,
 NAME,
 died,
--- new_table/step_22_ver_1.sql
+++ new_table/step_22_ver_1.sql
@@ -1,4 +1,4 @@
-UPDATE reg_03_14_23_vw
-SET act = 'March 14, 2023';
+UPDATE reg_03_20_23_vw
+SET act = 'March 20, 2023';
 
-SELECT * FROM reg_03_14_23_vw;	
+SELECT * FROM reg_03_20_23_vw;	
