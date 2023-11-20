ba7b8db2e9a8cb0981d2c3c8dcfb5f5acd8a32cf
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2023-11-20 14:03:33 +0200

    update

--- new_table/step_15_ver_1.sql
+++ new_table/step_15_ver_1.sql
@@ -1,4 +1,4 @@
-INSERT INTO reg_11_14_23_t1 (
+INSERT INTO reg_11_20_23_t1 (
 id,
 NAME,
 died,
--- new_table/step_7_ver_1.sql
+++ new_table/step_7_ver_1.sql
@@ -12,7 +12,7 @@ DATE_OF_LIQUID,
 STATUTUL_DATE,
 STATUTUL
 
-FROM registru_md_11_14_2023
+FROM registru_md_11_20_2023
 
 
 
