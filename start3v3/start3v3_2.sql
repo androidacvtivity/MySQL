5400a8aab9d8eaf61e3d88d4239682157ebbdc85
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2024-10-16 09:00:00 +0300

    Update start3v3_2.sql

--- start3v3/start3v3_2.sql
+++ start3v3/start3v3_2.sql
@@ -1,2 +1,21 @@
-SELECT  *
+SELECT  distinct star
+FROM start3v3;
+
+
+SELECT distinct serviciu
+FROM start3v3;
+
+
+SELECT *
 FROM start3v3
+WHERE
+-- NAME LIKE '%Elvina%'
+-- OR 
+depart LIKE '%statistica agriculturii si mediului%'
+;
+
+
+SELECT *
+FROM start3v3
+WHERE
+1
