1de02cd8c51082f85e7c6a4146d009249aa37fff
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-07-31 09:08:21 +0300

    31-1 #523

---
+++ start3v5/update_1.sql
@@ -0,0 +1,58 @@
+UPDATE start3v5
+SET 
+  name = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(name,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+  
+  statut = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(statut,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  description = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(description,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  galaxy = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(galaxy,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  star = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(star,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  dob = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(dob,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  serviciu = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(serviciu,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  sectia = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(sectia,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  depart = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(depart,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  phone = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(phone,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  phoneinternal = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(phoneinternal,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  email = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(email,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  personalinfo = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(personalinfo,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  formname = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(formname,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  phonemobil = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(phonemobil,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  floor = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(floor,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  office = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(office,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  notice = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(notice,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a'),
+
+  studio = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(studio,
+        'Ţ','T'),'ţ','t'),'Ș','S'),'ș','s'),'Ş','S'),'ş','s'),'Ă','A'),'ă','a'),'Â','A'),'â','a');
