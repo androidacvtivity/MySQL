11f4060ef19a627c3e1b1069fa11aa6957fdd802
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-10-27 08:53:29 +0200

    Update company-import-template.sql

--- _13_05_2025_work/company-import-template.sql
+++ _13_05_2025_work/company-import-template.sql
@@ -1,5 +1,5 @@
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/company-2025.09.29.csv'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/company-2025.10.13.csv'
 INTO TABLE registru_md_09_29_2025
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' 
